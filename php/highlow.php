04dedf955a0c23838cb4684c239c681652a24774
Alex Guerra <lovelyVector@gmail.com>
2014-02-07 10:31:04 -0600

    Added the hilo documentation and the greatting

--- php/highlow.php
+++ php/highlow.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * highlow
+ *
+ * A simple game that guesses numbers and is able to take user input.
+ *
+ * The game will ask the player for there name.
+ *
+ * @author Alexander A Guerra <lovelyVector@gmail.com>
+ * @version 0.0.1
+ * @copyright lovelyVector 2014
+ */
 
+
+fwrite(STDOUT, "**********************\n");
+fwrite(STDOUT, "** Welcome to Hi-lo **\n");
+fwrite(STDOUT, "**********************\n");
+
+
+
+fwrite(STDOUT, 'What is your first name? ');
+
+
+$first_name = fgets(STDIN);
+
+fwrite(STDOUT, "Hello $first_name");
 
 ?>
