0c8d0faf82e20cb6f5a7d71584df6fcba34ab73e
Alex Guerra <lovelyVector@gmail.com>
2014-02-09 17:32:22 -0600

    Did some type juggling to make the user entered values an integer.

--- php/for.php
+++ php/for.php
@@ -11,11 +11,11 @@
 
 // Prompt the user for a starting number and store the response in $minNumber.
 fwrite(STDOUT, "Please enter a number.\n");
-$minNumber = fgets(STDOUT);
+$minNumber = (int) fgets(STDOUT);
 
 // Prompt the user for an ending number and store the response in $maxNumber.
 fwrite(STDOUT, "Please enter a number.\n");
-$maxNumber = fgets(STDOUT);
+$maxNumber = (int)fgets(STDOUT);
 
 // Now that I have two numbers what needs to be checked?
 // I need to check that both numbers are not the same.
@@ -25,6 +25,12 @@ $maxNumber = fgets(STDOUT);
 // At this point I am generating an infinite loop...I think it's because I am using a string as my minNumber.
 // I should probably cast it to an int.
 
+echo var_dump($minNumber);
+if(!is_numeric($minNumber) || !is_numeric($maxNumber))
+{
+	echo "Please enter values that are numbers\n";
+	exit(0);
+}
 
 // I am not usre if this is the right way for me to approch the whole problem.
 // But I willl keep on moving on...That is the only way for me to go. Forward...
