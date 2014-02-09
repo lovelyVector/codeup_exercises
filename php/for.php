68982c558258a89c81710b23febb659ecd328621
Alex Guerra <lovelyVector@gmail.com>
2014-02-09 15:25:52 -0600

    Add documentation and user prompt for a low number

--- php/for.php
+++ php/for.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * for.php
+ *
+ * This is information about the for.php file.
+ * The excersise first asks the user to be prompted for a starting and ending number.
+ * 
+ * @author Alexander A Guerra <lovelyVector@gmail.com>
+ * @version 0.0.1
+ */
+
+// Prompt the user for a starting number and store the response in minNumber.
+fwrite(STDOUT, "Please enter a number.\n");
+$minNumber = fgets(STDOUT);
 
 
 ?>
