0878d9968939326754406b490a4d6c7a978de762
Alex Guerra <lovelyVector@gmail.com>
2014-02-09 16:58:47 -0600

    Add comments that include the coding objective

---
+++ php/fizzbuzz.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * fizzbuzz
+ *
+ * The fizzbuzz program should print out numbers 1 - 100.
+ * If the number is a multiple of three it should print the word "Fizz".
+ * If the number is a multiple of five it should print the word "Buzz".
+ * If the number is a multiple of both 3 and 5 it should print the word, "FizzBuzz".
+ * 
+ * @author Alexander A Guerra <lovelyVector@gmail.com>
+ * @version 0.0.1
+ */
+
+
+?>
