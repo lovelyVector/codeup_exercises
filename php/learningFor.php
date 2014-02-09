1f031943296bc9f5cf9d1f80c2fd8958c1bc3874
Alex Guerra <lovelyVector@gmail.com>
2014-02-09 15:08:26 -0600

    Added comments that include how to use the for loop

--- php/learningFor.php
+++ php/learningFor.php
@@ -1,5 +1,19 @@
 <?php
 
-
+/**
+ * learningFor.php
+ *
+ * This is a scratch file that goes along with the control structures 2 part one.
+ * How to think about the for loop?
+ * The keyword to start a for loop is "for"
+ * The for loop takes 3 arguments.
+ * One thing to remember about the for loop is that the arguments are seperated by a semi-colon not a comma.
+ * The first argument of the for loop is run only the first time the for loop is encountored.
+ * The second argument is the conditional that is checked to see if the for loop is run. It must be true to run.
+ * The third argument is exicuted after the each pass of the for loop.
+ * 
+ * @author Alexander A Guerra <lovelyVector@gmail.com>
+ * @version 0.0.1
+ */
 
 ?>
