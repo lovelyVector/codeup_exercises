4fce1b2a0046290adbd8c2ed7c3a4a587f7bbe9c
Alex Guerra <lovelyVector@gmail.com>
2014-02-10 10:41:39 -0600

    basic array testing

--- php/foreach.php
+++ php/foreach.php
@@ -13,6 +13,17 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-	# code...
+	
+
+	if (is_array($thing)) {
+
+		foreach ($thing as $arrayObject) {
+			echo "$arrayObject ";
+			# code...
+		}
+	}
+	else{
+		echo "$thing\n";
+	}
 }
 ?>
