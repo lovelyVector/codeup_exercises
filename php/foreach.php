d7938ef1961c2bd94ddf8f28b8611e881796255b
Alex Guerra <lovelyVector@gmail.com>
2014-02-10 10:31:49 -0600

    Working through the examples

--- php/foreach.php
+++ php/foreach.php
@@ -10,5 +10,18 @@
  * @version 0.0.1
  */
 
+/**
+ * @var array an array of fruits
+ */
+$fruits = array('apple','banana','orange','lime');
+
+for ($i=0; $i < count($fruits); $i++) { 
+	echo ("\$fruits has an element with a value of {$fruits{$i}}\n");
+}
+
+foreach ($fruits as $fruit) {
+	echo "$fruit";
+	echo "\n";
+}
 
 ?>
