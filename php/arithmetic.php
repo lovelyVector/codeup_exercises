c11242044051b13f782e3047a863c105b53a24e6
Alex Guerra <lovelyVector@gmail.com>
2014-02-11 11:05:00 -0600

    Inital commit of arithmetic.php

---
+++ php/arithmetic.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Spooon.php
+ *
+ * This is a file to hold things.
+ *
+ * @package MyProject
+ * @subpackage Common
+ * @license http://opensource.org/licenses/MIT The MIT License
+ * @author Alexander A Guerra <lovelyVector@gmail.com>
+ * @version 0.0.1
+ */
+
+/**
+ * This function adds two numbers together
+ */
+function add($a, $b) {
+    echo $a + $b;
+}
+
+/**
+ * This function subtracks two numbers.
+ */
+function subtract($a, $b) {
+    echo$a - $b;
+}
+
+function multiply($a, $b) {
+    echo $a * $b;
+}
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+
+?>
